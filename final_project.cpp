b055e14a154fa9e343d3129e140b3d4acf8e2798
hassanmirza28 <mirzahasan201@gmail.com>
2025-01-26 23:02:32 +0500

    function added by hassan

--- final_project.cpp
+++ final_project.cpp
@@ -22,3 +22,23 @@ string phoneNumber;
 int age;
 string flyingClass;
 };
+
+void viewBookingRecords() {
+ifstream file("bookings.txt");
+if (!file) {
+cout << "No booking records found.\n";
+return;
+}
+cout << "\nBooking Records:\n";
+cout << "Flight Number Name Booking Date Phone Number Age Flying
+Class\n";
+cout << "--------------------------------------------------------------------------------------------\n";
+Passenger p;
+while (file >> p.flightNumber >> p.name >> p.bookingDate >> p.phoneNumber >> p.age >>
+p.flyingClass) {
+cout << p.flightNumber << " " << p.name << " "
+<< p.bookingDate << " " << p.phoneNumber << " "
+<< p.age << " " << p.flyingClass << "\n";
+}
+file.close();
+}
